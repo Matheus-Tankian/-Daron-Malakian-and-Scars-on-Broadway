8175cca1895918198f7cd2efcb7dece60db744c2
Matheus <matheusvitor8143@gmail.com>
2020-09-24 17:27:50 -0300

    frist commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//Automatic Slideshow - change image every 4 seconds
+
+var myIndex = 0;
+carousel();
+
+function carousel(){
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for( i = 0; i < x.length; i++){
+        x[i].style.display = "none";
+    }
+    myIndex++;
+
+    if(myIndex > x.length){
+        myIndex  = 1;
+    }
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 4000);
+}
+
+function myFunction(){
+    var x = document.getElementById('ticketModal');
+    window.onclick = function(event){
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+    }
+}
